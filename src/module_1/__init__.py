073f348ca71e0d1012fd679ea683b46858e3b00a
Indiwara65 <indiwarananayakkara1@gmail.com>
2025-06-17 21:10:37 +0530

    code added to module_1 and module_2

--- src/module_1/__init__.py
+++ src/module_1/__init__.py
@@ -1,3 +1,9 @@
+#This script is optional
+#If this script is not used functions will be needed to be imported as show in 1 else 2
+# 1. from module_1.additions import add
+# 2. from module_1 import add
 __version__ = "1.0.0"
 
-__all__ = []
+from .additions import add
+
+__all__ = [add]
--- src/module_2/__init__.py
+++ src/module_2/__init__.py
@@ -1,3 +1,5 @@
 __version__ = "1.0.0"
 
-__all__ = []
+from .substractions import sub
+
+__all__ = [sub]
