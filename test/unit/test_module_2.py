bbe74bd1afa2ba65448e30adba8009c59bf6a3ba
Indiwara65 <127720818+Indiwara65@users.noreply.github.com>
2025-06-21 18:50:19 +0530

    Update test_module_2.py

--- test/unit/test_module_2.py
+++ test/unit/test_module_2.py
@@ -4,5 +4,6 @@ from module_2 import sub
 
 def test_sub():
     assert sub(9,0)==9
+    assert sub(10,2)==8
     assert sub(10,12.3)==pytest.approx(-2.3, rel=1e-5)
     assert sub(-10.11,14.13)==pytest.approx(-24.24, rel=1e-5)
