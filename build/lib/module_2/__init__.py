d88e46f429ab69d10dd2c3186c8d7a2768c7b4c6
Indiwara65 <indiwarananayakkara1@gmail.com>
2025-06-21 07:05:02 +0530

    updated pyproject.toml

---
+++ build/lib/module_2/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.0.0"
+
+from .substractions import sub
+
+__all__ = [sub]
---
+++ test/unit/test_module_1.py
@@ -0,0 +1,8 @@
+import pytest
+
+from module_1 import add
+
+def test_add():
+    assert add(9,0)==9
+    assert add(10,12.3)==22.3
+    assert add(-10.11,14.13)==pytest.approx(4.02)
