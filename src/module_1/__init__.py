fcd630c1bf043a84c300ebfe614e95c0f922a5a3
Indiwara65 <indiwarananayakkara1@gmail.com>
2025-07-09 14:56:52 +0530

    last update of feature branch

--- src/module_1/__init__.py
+++ src/module_1/__init__.py
@@ -1,5 +1,5 @@
-#This script is optional
-#If this script is not used functions will be needed to be imported as show in 1 else 2
+# This script is optional
+# If this script is not used functions will be needed to be imported as show in 1 else 2
 # 1. from module_1.additions import add
 # 2. from module_1 import add
 __version__ = "1.0.0"
